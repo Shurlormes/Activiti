16eb464b5d4479861bad3b8adaf2df266f7989d7
Shurlormes <tsdszh@outlook.com>
2018-07-04 17:29:55 +0800

    dynamic model test

--- src/test/java/BpmnModelTest.java
+++ src/test/java/BpmnModelTest.java
@@ -29,21 +29,20 @@ public class BpmnModelTest {
     public void test() {
         Deployment deployment = repositoryService.createDeployment()
             .key("Model From Bpmn Key").name("Model From Bpmn")
-            .addBpmnModel("Model From Bpmn", this.generateBpmnModel()).deploy();
+            .addBpmnModel("dynamic-model.bpmn", this.generateBpmnModel()).deploy();
 
         System.out.println(deployment.getId());
 
         ProcessDefinition processDefinition = repositoryService.createProcessDefinitionQuery()
             .deploymentId(deployment.getId()).singleResult();
 
+        System.out.println(processDefinition);
         //runtimeService.startProcessInstanceById(processDefinition.getId());
     }
 
     @Test
     public void test2() {
-        ProcessDefinition processDefinition = repositoryService.createProcessDefinitionQuery()
-            .deploymentId("45001").singleResult();
-        runtimeService.startProcessInstanceById(processDefinition.getId());
+        runtimeService.startProcessInstanceById("BpmnModelId:1:67503");
     }
 
 
@@ -51,7 +50,7 @@ public class BpmnModelTest {
         BpmnModel bpmnModel = new BpmnModel();
 
         Process process = new Process();
-        process.setId("BpmnModel timer");
+        process.setId("BpmnModelId");
         process.setName("First BpmnModel");
 
         StartEvent startEvent = new StartEvent();
@@ -59,7 +58,7 @@ public class BpmnModelTest {
         process.addFlowElement(startEvent);
 
         UserTask userTask = new UserTask();
-        userTask.setId("BpmnModel Task Id");
+        userTask.setId("BpmnModelTaskId");
         userTask.setName("BpmnModel Task");
 
         process.addFlowElement(userTask);
@@ -68,13 +67,88 @@ public class BpmnModelTest {
         endEvent.setId("endEvent");
         process.addFlowElement(endEvent);
 
-        process.addFlowElement(new SequenceFlow("startEvent", "BpmnModel Task Id"));
-        process.addFlowElement(new SequenceFlow("BpmnModel Task Id", "endEvent"));
+        process.addFlowElement(new SequenceFlow("startEvent", "BpmnModelTaskId"));
+        process.addFlowElement(new SequenceFlow("BpmnModelTaskId", "endEvent"));
 
 
         bpmnModel.addProcess(process);
 
 
         return bpmnModel;
+    }
+
+
+    @Test
+    public void testDynamicDeploy() throws Exception {
+        // 1. Build up the model from scratch
+        BpmnModel model = new BpmnModel();
+        Process process = new Process();
+        model.addProcess(process);
+        process.setId("my-process");
+
+        process.addFlowElement(createStartEvent());
+        process.addFlowElement(createUserTask("task1", "First task", "fred"));
+        process.addFlowElement(createUserTask("task2", "Second task", "john"));
+        process.addFlowElement(createEndEvent());
+
+        process.addFlowElement(createSequenceFlow("start", "task1"));
+        process.addFlowElement(createSequenceFlow("task1", "task2"));
+        process.addFlowElement(createSequenceFlow("task2", "end"));
+
+        // 2. Generate graphical information
+        //new BpmnAutoLayout(model).execute();
+
+        // 3. Deploy the process to the engine
+        Deployment deployment = repositoryService.createDeployment()
+            .addBpmnModel("dynamic-model.bpmn", model).name("Dynamic process deployment")
+            .deploy();
+
+        // 4. Start a process instance
+        //ProcessInstance processInstance = runtimeService.startProcessInstanceByKey("my-process");
+
+        // 5. Check if task is available
+//        List<Task> tasks = taskService.createTaskQuery()
+//            .processInstanceId(processInstance.getId()).list();
+//
+//        Assert.assertEquals(1, tasks.size());
+//        Assert.assertEquals("First task", tasks.get(0).getName());
+//        Assert.assertEquals("fred", tasks.get(0).getAssignee());
+
+        // 6. Save process diagram to a file
+//        InputStream processDiagram = repositoryService.getProcessDiagram(processInstance.getProcessDefinitionId());
+//        FileUtils.copyInputStreamToFile(processDiagram, new File("target/diagram.png"));
+
+        // 7. Save resulting BPMN xml to a file
+//        InputStream processBpmn = activitiRule.getRepositoryService()
+//            .getResourceAsStream(deployment.getId(), "dynamic-model.bpmn");
+//        FileUtils.copyInputStreamToFile(processBpmn,
+//            new File("target/process.bpmn20.xml"));
+    }
+
+    public UserTask createUserTask(String id, String name, String assignee) {
+        UserTask userTask = new UserTask();
+        userTask.setName(name);
+        userTask.setId(id);
+        userTask.setAssignee(assignee);
+        return userTask;
+    }
+
+    public SequenceFlow createSequenceFlow(String from, String to) {
+        SequenceFlow flow = new SequenceFlow();
+        flow.setSourceRef(from);
+        flow.setTargetRef(to);
+        return flow;
+    }
+
+    public StartEvent createStartEvent() {
+        StartEvent startEvent = new StartEvent();
+        startEvent.setId("start");
+        return startEvent;
+    }
+
+    public EndEvent createEndEvent() {
+        EndEvent endEvent = new EndEvent();
+        endEvent.setId("end");
+        return endEvent;
     }
 }
